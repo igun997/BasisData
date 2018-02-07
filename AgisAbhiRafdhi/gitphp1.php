7b3be75d352c1ed8c67d0feaa96f4e3e51f4076d
agis abhi rafdhi <agisabhi28@email.unikom.ac.id>
2018-02-07 13:33:45 +0700

    tugas if else dan looping for

---
+++ AgisAbhiRafdhi/gitphp1.php
@@ -0,0 +1,20 @@
+<?php
+$t = ("80");
+if($t>=80){
+  echo "Nilai $t = A <br>";
+}else if($t>=69){
+  echo "Nilai $t = B <br>";
+}else if($t>=50){
+  echo "Nilai $t = C <br>";
+}else if($t>=40){
+  echo "Nilai $t = D <br>";
+}else{
+  echo "Nilai $t = E <br>";
+}
+ ?>
+
+<?php
+for($x=1;$x<=10;$x++){
+  echo "Angka $x <br>";
+}
+?>
