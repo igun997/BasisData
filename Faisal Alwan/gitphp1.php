1b2a13c2b3afeeba374c3a945f78be49d93b1301
faisal alwan <f.alwan9@email.unikom.ac.id>
2018-02-07 14:04:45 +0700

    TUGAS PHP

---
+++ Faisal Alwan/gitphp1.php
@@ -0,0 +1,20 @@
+<?php
+echo "IF ELSE<br>";
+  $tahun=("2016");
+  if($tahun%4==0)
+  {
+    echo "$tahun Adalah Tahun Kabisat <br>";
+  }
+  else
+  {
+    echo "$tahun Bukan Tahun Kabisat<br>";
+  }
+ ?>
+
+
+ <?php
+ echo "<br>LOOPING<br>";
+ for ($i=10; $i >=1 ; $i--) {
+   echo "Angka ke $i <br>";
+ }
+  ?>
