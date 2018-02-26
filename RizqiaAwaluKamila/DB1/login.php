86f10f789d66448b7f0578f8054defa110bce9fb
Rizqia Awalu Kamila <rizqia.akamila@email.ac.id>
2018-02-27 06:00:26 +0700

    nitip dulu

---
+++ RizqiaAwaluKamila/DB1/login.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+  <h2>Login</h2>
+  <form action="login.php" method="post">
+    <table>
+        <tr>
+          <td>Username :</td>
+          <td><input type="text" name="nama"></td>
+        </tr>
+        <tr>
+          <td>Passord :</td>
+          <td><input type="text" name="alamat"></td>
+        </tr>
+        <tr>
+          <td>
+          <a href="tambah_akun.html">Daftar Akun</a></br>
+          </td>
+        </tr>
+        <tr>
+          <td>
+          <input type="submit" value="Login">
+          </td>
+        </tr>
+    </table>
+  </form>
+</body>
+</html>
---
+++ RizqiaAwaluKamila/DB1/user.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+  <h2>Login</h2>
+  <h4>Halo Selamat Datang</h4>
+  <a href="login.php">Keluar</a></br>
+</body>
+</html>
